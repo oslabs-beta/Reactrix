db56b5e6597fd9c3caebfa270da779e8a12cb753
Kevin Chung <kevin@Kevins-MacBook-Pro.local>
2021-12-30 17:13:54 -0800

    boilerplate updated Co-authored-by: Jonatham Tam jktam336@gmail.com Co-authored-by: Tychicus Lee tychicuslee0215@gmail.com Co-authored-by: Jay Heo heojay0309@gmail.com

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,15 @@ import { hot } from 'react-hot-loader/root';
 import Button from '@material-ui/core/Button';
 
 interface Props {
-  name: string;
+  message: string;
 }
 
-class App extends React.Component<Props> {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>Hello {name}</h1>
-        <Button variant='contained'>PAY ATTENTIONOEIFEORIFJEIOFJEOIERJF</Button>
-      </>
-    );
-  }
-}
+const App = ({ message }: Props) => (
+  <div>
+    <Button variant='outlined' size='small'>
+      {message}
+    </Button>
+  </div>
+);
 
 export default hot(App);
--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,5 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './styles.css';
 
-var mountNode = document.getElementById('app');
-ReactDOM.render(<App name='Jane' />, mountNode);
+const root = document.getElementById('root');
+ReactDOM.render(<App message='test' />, root);
