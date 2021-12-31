2e18c54c819f80af886e371f622b2136cd9f1266
Kevin Chung <kevin@Kevins-MacBook-Pro.local>
2021-12-30 18:55:02 -0800

    added client and server folders

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import App from './App';
-import './styles.css';
+import App from './client/App';
+import './client/styles.css';
 
 const root = document.getElementById('root');
 ReactDOM.render(<App message='test' />, root);
