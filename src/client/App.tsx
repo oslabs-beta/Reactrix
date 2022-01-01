851cdcfba2ea24dae683d39f27395246b7638627
Tyke Lee <tyke@Tykes-MacBook-Air.local>
2021-12-31 17:50:38 -0800

    componentDetails

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
 import ComponentTree from './containers/ComponentTree';
 import ComponentLibrary from './containers/ComponentLibrary';
-
 import ComponentDetails from './containers/ComponentDetails';
+
+import Navbar from './components/Navbar';
 
 interface Props {
   message: string;
@@ -11,15 +12,11 @@ interface Props {
 
 const App = ({ message }: Props) => (
   <div>
-    <Button variant='outlined' size='small'>
-      {message}
-    </Button>
-    <ComponentLibrary message='Hi'/>
-    
+    <Navbar />
+    <ComponentTree message='Bye' />
+    <ComponentLibrary message='Hi' />
     <ComponentDetails message='theDEETS'/>
   </div>
 );
-
-
 
 export default hot(App);
