13a7e94dcd546400a97e18bbc6cf8b6d3c237662
Tyke Lee <tyke@Tykes-MacBook-Air.local>
2022-01-06 14:57:45 -0800

    models edit

--- src/server/models/reactrixModels.tsx
+++ src/server/models/reactrixModels.tsx
@@ -7,26 +7,11 @@ const pool = new Pool({
   connectionString: PG_URI
 });
 
+
+
 module.exports = {
   query: (text, params, callback) => {
     console.log('executed query', text);
     return pool.query(text, params, callback);
   }
 };
-
-
-// CREATE TABLE users (
-// 	id SERIAL PRIMARY KEY,
-// 	username varchar(255) NOT NULL,
-// 	password varchar(255) NOT NULL,
-// 	snapshots_id varchar(255) NOT NULL,
-// );
-
-// CREATE TABLE `snapshots` (
-// 	`id` numeric NOT NULL AUTO_INCREMENT,
-// 	PRIMARY KEY (`id`)
-// );
-
-// ALTER TABLE `users` ADD CONSTRAINT `users_fk0` FOREIGN KEY (`snapshots_id`) REFERENCES `snapshots`(`id`);
-
-
