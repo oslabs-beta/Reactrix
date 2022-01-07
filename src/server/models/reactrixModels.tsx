64f2f5d9b4b303baba46094507bdf39ceee9ecda
Tyke Lee <tyke@Tykes-MacBook-Air.local>
2022-01-07 11:18:42 -0800

    database model

--- src/server/models/reactrixModels.tsx
+++ src/server/models/reactrixModels.tsx
@@ -10,7 +10,7 @@ const pool = new Pool({
 
 
 module.exports = {
-  query: (text, params, callback) => {
+  query: (text: any, params: any, callback: any) => {
     console.log('executed query', text);
     return pool.query(text, params, callback);
   }
